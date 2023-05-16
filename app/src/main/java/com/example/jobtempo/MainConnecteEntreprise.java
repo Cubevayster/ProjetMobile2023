edc72d3329ec9c7ed5366bbfd81a989b2c7ef33f
Flo <florian.thepaut34@gmail.com>
2023-05-16 12:01:26 +0200

    début main entreprise

--- app/src/main/java/com/example/jobtempo/MainConnecteEntreprise.java
+++ app/src/main/java/com/example/jobtempo/MainConnecteEntreprise.java
@@ -1,6 +1,32 @@
 package com.example.jobtempo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainConnecteEntreprise extends Activity {
+
+    Button monCompte;
+    Button gestionOffres;
+    Button deconnexion;
+    Button ajoutOffre;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_connecte_entreprise);
+
+        monCompte = findViewById(R.id.mon_compte_entreprise);
+        gestionOffres = findViewById(R.id.gestion_offre);
+        deconnexion = findViewById(R.id.deconnexion_entreprise);
+        ajoutOffre = findViewById(R.id.ajout_offre);
+
+        ajoutOffre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
