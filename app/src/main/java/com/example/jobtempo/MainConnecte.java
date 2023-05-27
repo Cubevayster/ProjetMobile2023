646aac9d2dff0b5a1a1bf107f3c27729c1a4349e
Cubevayster <arthurvillaroya@gmail.com>
2023-05-27 19:17:50 +0200

    Recerche Emploie

--- app/src/main/java/com/example/jobtempo/MainConnecte.java
+++ app/src/main/java/com/example/jobtempo/MainConnecte.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +24,11 @@ import com.google.firebase.database.ValueEventListener;
 public class MainConnecte extends AppCompatActivity {
     Button deconnexion;
     Button monCompte;
+    Button recherche;
     TextView utilisateurNom;
+
+    EditText lieu;
+    EditText metier;
 
     LinearLayout layoutScrollViewChercheur;
     FirebaseUser user;
@@ -40,7 +45,13 @@ public class MainConnecte extends AppCompatActivity {
         monCompte = findViewById(R.id.mon_compte);
         utilisateurNom = findViewById(R.id.utilisateur_nom);
         layoutScrollViewChercheur = findViewById(R.id.layoutScrollViewChercheur);
+        lieu = findViewById(R.id.lieu_travail);
+        metier = findViewById(R.id.metier_cible);
+        recherche = findViewById(R.id.recherche);
         user = auth.getCurrentUser();
+
+        final String[] Lieu = new String[1];
+        final String[] Metier = new String[1];
 
         DatabaseReference userRef = FirebaseDatabase.getInstance("https://jobtempo-2934d-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Users").child(user.getUid());
 
@@ -84,6 +95,66 @@ public class MainConnecte extends AppCompatActivity {
             }
         });
 
+        recherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Lieu[0] = String.valueOf(lieu.getText());
+                Metier[0] = String.valueOf(metier.getText());
+
+                DatabaseReference offresRef = FirebaseDatabase.getInstance("https://jobtempo-2934d-default-rtdb.europe-west1.firebasedatabase.app").getReference("Offers");
+                offresRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // Effacer le contenu de la ScrollView
+                        layoutScrollViewChercheur.removeAllViews();
+
+                        // Parcourir les offres d'emploi
+                        for(DataSnapshot offreSnapshot : snapshot.getChildren()) {
+                            // Récupérer les informations de l'offre d'emploi
+                            String nomOffre = offreSnapshot.child("nomOffre").getValue(String.class);
+                            String lieuOffre = offreSnapshot.child("localisationString").getValue(String.class);
+                            String offreId = offreSnapshot.getKey();
+                            if(nomOffre.contains(Metier[0]) && lieuOffre.contains(Lieu[0])) {
+                                // Créer un bouton pour afficher le nom de l'offre d'emploi
+                                Button offreButton = new Button(getApplicationContext());
+                                offreButton.setText(nomOffre);
+                                offreButton.setTag(offreId);
+
+                                offreButton.setBackgroundResource(R.drawable.btn_bg);
+
+                                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                                        LinearLayout.LayoutParams.MATCH_PARENT,
+                                        LinearLayout.LayoutParams.WRAP_CONTENT
+                                );
+                                int margin = getResources().getDimensionPixelSize(R.dimen.button_margin); // Récupérer la valeur de la marge à partir des ressources
+                                layoutParams.setMargins(margin, margin, margin, margin);
+                                offreButton.setLayoutParams(layoutParams);
+
+                                offreButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        // Récupérer l'identifiant de l'offre d'emploi
+                                        String clickedOffreId = (String) view.getTag();
+
+                                        // Passer l'identifiant de l'offre d'emploi à l'activité RecapOffre
+                                        Intent intent = new Intent(getApplicationContext(), RecapOffreChercheur.class);
+                                        intent.putExtra("offreId", clickedOffreId);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                                layoutScrollViewChercheur.addView(offreButton);
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainConnecte.this, "Erreur : " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
 
 
         DatabaseReference offresRef = FirebaseDatabase.getInstance("https://jobtempo-2934d-default-rtdb.europe-west1.firebasedatabase.app").getReference("Offers");
@@ -97,8 +168,8 @@ public class MainConnecte extends AppCompatActivity {
                 for(DataSnapshot offreSnapshot : snapshot.getChildren()) {
                     // Récupérer les informations de l'offre d'emploi
                     String nomOffre = offreSnapshot.child("nomOffre").getValue(String.class);
-                    String offreId = offreSnapshot.getKey();
 
+                    String offreId = offreSnapshot.getKey();
                     // Créer un bouton pour afficher le nom de l'offre d'emploi
                     Button offreButton = new Button(getApplicationContext());
                     offreButton.setText(nomOffre);
